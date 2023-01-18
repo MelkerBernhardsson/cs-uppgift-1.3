1007d0aad23ae201482e173a890d8ff77957e9dc
Pingvu <nhlpool27@gmail.com>
2023-01-18 10:20:50 +0100

    Add project files.

---
+++ cs uppgift 1.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uppgift3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();    
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" / \"\" \\");
+            Console.WriteLine("/______\\");
+        }
+    }
+}
